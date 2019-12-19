5221f5f8b80902135192c0cb5b5712601624acbf
Jonathan XERRI <jxerri145@gmail.com>
2019-12-19 16:53:23 +0100

    docs

--- serverwss.php
+++ serverwss.php
@@ -1,46 +1,65 @@
-<?php require __DIR__.'/vendor/autoload.php';
+<?php require __DIR__ . '/vendor/autoload.php';
 
-use Ratchet\Server\IoServer;
-use Ratchet\Http\HttpServer;
-use Ratchet\WebSocket\WsServer;
-use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
-use Illuminate\Support\Facades\Auth;
+use Ratchet\Http\HttpServer;
+use Ratchet\MessageComponentInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
 
 define('APP_PORT', 8088);
 
-class ServerImpl implements MessageComponentInterface {
+class ServerImpl implements MessageComponentInterface
+{
     protected $clients;
 
-    public function __construct() {
+    /*
+     * Le constructeur du serveur
+     */
+    public function __construct()
+    {
         //Auth::user();
         //$this->middleware('auth');
         $this->clients = new \SplObjectStorage;
     }
-
-    
-    public function onOpen(ConnectionInterface $conn) {
+    /**
+     * WebSocket - Fonction OnOpen
+     *
+     * Connexion à la base de données pour pouvoir effectuer différentes requêtes
+     * Le serveur se connecte au client, puis envoi les informations au même client afin de tester différentes clés
+     * @param ConnectionInterface $conn : Connexion à l'interface
+     * @return Un message disant qu'un utilisateur est connecté
+     */
+    public function onOpen(ConnectionInterface $conn)
+    {
         $dbLink = mysqli_connect('mysql-dams.alwaysdata.net', 'dams', 'lemdpbasetest')
         or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 
         mysqli_select_db($dbLink, 'dams_lumen')
         or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
-        
+
         $this->clients->attach($conn);
         $row = "1";
-        
+
         $verification = "SELECT Message FROM Message WHERE IdMessage = $row"; //Vérifie les données rentrées
         if ($resultatverification = mysqli_query($dbLink, $verification)) {
             $row = mysqli_fetch_assoc($resultatverification);
         }
         $arr = array('chaine' => $row['Message'],
-        'choix' => 1, 'pos' => 1, 'mode' => 1, 'fin' => 6);
+            'choix' => 1, 'pos' => 1, 'mode' => 1, 'fin' => 6);
         $msg = json_encode($arr);
         $conn->send($msg);
         echo "New connection! ({$conn->resourceId}).\n";
     }
 
-    public function onMessage(ConnectionInterface $conn, $msg) {
+    /**
+     * WebSocket - Fonction OmMessage
+     *
+     * Message qui sera envoyé pour confirmer la relation client - serveur
+     * @param ConnectionInterface $conn : Connexion à l'interface
+     * @param string $msg : Message à envoyer
+     */
+    public function onMessage(ConnectionInterface $conn, $msg)
+    {
         echo sprintf("New message from '%s': %s\n\n\n", $conn->resourceId, $msg);
         foreach ($this->clients as $client) { // BROADCAST
             $message = json_decode($msg, true);
@@ -50,12 +69,27 @@ class ServerImpl implements MessageComponentInterface {
         }
     }
 
-    public function onClose(ConnectionInterface $conn) {
+    /**
+     * WebSocket - Fonction onClose
+     *
+     * Permet de déconnecter la relation client serveur existante
+     * @param ConnectionInterface $conn : Connexion à l'interface
+     */
+    public function onClose(ConnectionInterface $conn)
+    {
         $this->clients->detach($conn);
         echo "Connection {$conn->resourceId} is gone.\n";
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
+    /**
+     * WebSocket - Fonction onError
+     *
+     * Permet de gérer les erreurs existantes
+     * @param ConnectionInterface $conn : Connexion à l'interface
+     * @param Exception $e : Exception
+     */
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
         echo "An error occured on connection {$conn->resourceId}: {$e->getMessage()}\n\n\n";
         $conn->close();
     }
